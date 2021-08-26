3dedc319229884dcda5b6241ffe2c9966e1cfa5c
fmarabini93 <fmarabini@icloud.com>
2021-08-26 10:20:26 +0200

    green-background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #00ff00;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;

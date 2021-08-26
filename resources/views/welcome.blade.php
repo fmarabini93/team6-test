bebaf958aec29b81b910cbcc42d260e9af86a698
Stefano-Para <stefano.zina@gmail.com>
2021-08-26 10:55:01 +0200

    bg blue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #00ff00;
+                background-color: blue;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
